2cd9dbf33ac814fa18535024c82e32ad8abd5f07
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-14 12:31:53 +0100

    Stories folder deleted, additional storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,38 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  staticDirs: ["../public"],
+  stories: [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          components: path.resolve(__dirname, "../components"),
+          atoms: path.resolve(__dirname, "../components/atoms"),
+          molecules: path.resolve(__dirname, "../components/molecules"),
+          organisms: path.resolve(__dirname, "../components/organisms"),
+          pages: path.resolve(__dirname, "../components/pages"),
+          context: path.resolve(__dirname, "../context"),
+          hooks: path.resolve(__dirname, "../hooks"),
+          resources: path.resolve(__dirname, "../resources"),
+          types: path.resolve(__dirname, "../types"),
+          utils: path.resolve(__dirname, "../utils"),
+        },
+      },
+    });
+
+    return config;
+  },
+};
