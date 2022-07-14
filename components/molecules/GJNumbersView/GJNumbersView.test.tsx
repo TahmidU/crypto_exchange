fd5592ae5e22436600e7f9048f10a7559313e0a6
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-14 16:55:13 +0100

    GJNumbersView test added

---
+++ components/molecules/GJNumbersView/GJNumbersView.test.tsx
@@ -0,0 +1,27 @@
+import { cleanup, render, screen } from "utils/test-config";
+
+import GJNumbersView from "./GJNumbersView";
+
+afterEach(cleanup);
+
+describe("GJNumbersView", () => {
+  test("show correct GJNumberLabels", () => {
+    // Given
+    const expectedTitle = "Test Title";
+    const counts: [number, string][] = [
+      [10, "Unique Users"],
+      [23, "Devices"],
+    ];
+    const expectedNumOfLabels = counts.length;
+    const gJNumberLabelTestId = "GJNumberLabel";
+
+    // When
+    render(<GJNumbersView title={expectedTitle} counts={counts} />);
+
+    // Then
+    expect(screen.getByText(expectedTitle)).toBeVisible();
+    expect(screen.getAllByTestId(gJNumberLabelTestId).length).toBe(
+      expectedNumOfLabels
+    );
+  });
+});
