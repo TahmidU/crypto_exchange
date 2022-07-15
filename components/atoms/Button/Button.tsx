6f74de7d02678fffaa1778968bc4c40732e058a2
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-15 10:55:55 +0100

    Button comp initial creation

---
+++ components/atoms/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface IButtonProps {}
+
+export default function Button({}: IButtonProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
