1acc8e043fd48e23026f7906cf7364e42d637b37
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-16 12:16:21 +0100

    Home page initial creation

---
+++ components/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface IHomePageProps {}
+
+export default function HomePage({}: IHomePageProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
