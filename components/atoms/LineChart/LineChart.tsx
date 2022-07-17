101c1133d21684556919024e05a9e6588918420f
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-17 15:03:05 +0100

    LineChart comp initial creation

---
+++ components/atoms/LineChart/LineChart.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface ILineChartProps {}
+
+export default function LineChart({}: ILineChartProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
